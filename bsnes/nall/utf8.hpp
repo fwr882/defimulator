9e606113e384904094368ddb9d00a356cc4159d1
Joe Brown <brownj3199@live.com>
2017-02-17 00:23:20 -0600

    Finished cleanup of top-level bsnes/nall folder

--- bsnes/nall/utf8.hpp
+++ bsnes/nall/utf8.hpp
@@ -16,69 +16,86 @@
 #undef interface
 
 namespace nall {
-  //UTF-8 to UTF-16
-  class utf16_t {
-  public:
-    operator wchar_t*() {
-      return buffer;
+/* UTF-8 to UTF-16 */
+class utf16_t {
+public:
+    operator wchar_t*()
+    {
+        return buffer;
     }
 
-    operator const wchar_t*() const {
-      return buffer;
+    operator const wchar_t*() const
+    {
+        return buffer;
     }
 
-    utf16_t(const char *s = "") {
-      if(!s) s = "";
-      unsigned length = MultiByteToWideChar(CP_UTF8, 0, s, -1, 0, 0);
-      buffer = new wchar_t[length + 1]();
-      MultiByteToWideChar(CP_UTF8, 0, s, -1, buffer, length);
+    utf16_t(const char *s = "")
+    {
+        if (!s) {
+            s = "";
+        }
+        unsigned length = MultiByteToWideChar(CP_UTF8, 0, s, -1, 0, 0);
+        buffer = new wchar_t[length + 1]();
+        MultiByteToWideChar(CP_UTF8, 0, s, -1, buffer, length);
     }
 
-    ~utf16_t() {
-      delete[] buffer;
+    ~utf16_t(void)
+    {
+        delete[] buffer;
     }
 
-  private:
+private:
     wchar_t *buffer;
-  };
+};
 
-  //UTF-16 to UTF-8
-  class utf8_t {
-  public:
-    operator char*() {
-      return buffer;
+/* UTF-16 to UTF-8 */
+class utf8_t {
+public:
+    operator char*()
+    {
+        return buffer;
     }
 
-    operator const char*() const {
-      return buffer;
+    operator const char*() const
+    {
+        return buffer;
     }
 
-    utf8_t(const wchar_t *s = L"") {
-      if(!s) s = L"";
-      unsigned length = WideCharToMultiByte(CP_UTF8, 0, s, -1, 0, 0, (const char*)0, (BOOL*)0);
-      buffer = new char[length + 1]();
-      WideCharToMultiByte(CP_UTF8, 0, s, -1, buffer, length, (const char*)0, (BOOL*)0);
+    utf8_t(const wchar_t *s = L"")
+    {
+        if (!s) {
+            s = L"";
+        }
+
+        unsigned length = WideCharToMultiByte(CP_UTF8, 0, s, -1, 0,
+            0, (const char*)0, (BOOL*)0);
+        buffer = new char[length + 1]();
+        WideCharToMultiByte(CP_UTF8, 0, s, -1, buffer, length,
+            (const char*)0, (BOOL*)0);
     }
 
-    ~utf8_t() {
-      delete[] buffer;
+    ~utf8_t(void)
+    {
+        delete[] buffer;
     }
 
     utf8_t(const utf8_t&) = delete;
     utf8_t& operator=(const utf8_t&) = delete;
 
-  private:
+private:
     char *buffer;
-  };
+};
 
-  inline void utf8_args(int &argc, char **&argv) {
+inline void utf8_args(int &argc, char **&argv)
+{
     wchar_t **wargv = CommandLineToArgvW(GetCommandLineW(), &argc);
     argv = new char*[argc];
-    for(unsigned i = 0; i < argc; i++) {
-      argv[i] = new char[_MAX_PATH];
-      strcpy(argv[i], nall::utf8_t(wargv[i]));
+    for (unsigned i = 0; i < argc; i++) {
+        argv[i] = new char[_MAX_PATH];
+        strcpy(argv[i], nall::utf8_t(wargv[i]));
     }
-  }
+}
+
 }
 
 #endif  //if defined(_WIN32)
