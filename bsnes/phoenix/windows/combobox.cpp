bf5886daa11a957e47c7c73c956460920b61da9d
Joe Brown <brownj3199@live.com>
2017-02-20 10:05:41 -0600

    Windows Pheonix cleanup.
    Not quite complete yet.  Eventually move everything to GTK+ if possible.

--- bsnes/phoenix/windows/combobox.cpp
+++ bsnes/phoenix/windows/combobox.cpp
@@ -1,46 +1,67 @@
-void ComboBox::create(Window &parent, unsigned x, unsigned y, unsigned width, unsigned height, const string &text) {
-  widget->window = CreateWindowEx(
-    0, L"COMBOBOX", L"",
-    WS_CHILD | WS_TABSTOP | WS_VISIBLE | CBS_DROPDOWNLIST | CBS_HASSTRINGS,
-    x, y, width, 200,
-    parent.widget->window, (HMENU)object->id, GetModuleHandle(0), 0
-  );
+void ComboBox::create(Window &parent, unsigned x, unsigned y, unsigned width,
+    unsigned height, const string &text)
+{
+    widget->window = CreateWindowEx(
+        0, L"COMBOBOX", L"",
+        WS_CHILD | WS_TABSTOP | WS_VISIBLE | CBS_DROPDOWNLIST | CBS_HASSTRINGS,
+        x, y, width, 200,
+        parent.widget->window, (HMENU)object->id, GetModuleHandle(0), 0
+    );
 
-  SetWindowLongPtr(widget->window, GWLP_USERDATA, (LONG_PTR)this);
-  SendMessage(widget->window, WM_SETFONT, (WPARAM)(parent.window->defaultFont ? parent.window->defaultFont : OS::os->proportionalFont), 0);
+    SetWindowLongPtr(widget->window, GWLP_USERDATA, (LONG_PTR)this);
+    if (parent.window->defaultFont) {
+        SendMessage(widget->window, WM_SETFONT,
+            (WPARAM)(parent.window->defaultFont), 0);
+    } else {
+        SendMessage(widget->window, WM_SETFONT,
+            (WPARAM)(OS::os->proportionalFont), 0);
+    }
 
-  //CreateWindow height parameter is the height of the expanded list box;
-  //need additional code to override default ComboBox control height
-  RECT rc;
-  GetWindowRect(widget->window, &rc);
-  unsigned adjustedHeight = height - ((rc.bottom - rc.top) - SendMessage(widget->window, CB_GETITEMHEIGHT, (WPARAM)-1, 0));
-  SendMessage(widget->window, CB_SETITEMHEIGHT, (WPARAM)-1, adjustedHeight);
+    /*
+    * CreateWindow height parameter is the height of the expanded list box;
+    * need additional code to override default ComboBox control height
+    */
+    RECT rc;
+    GetWindowRect(widget->window, &rc);
+    unsigned adjustedHeight = height - ((rc.bottom - rc.top) -
+        SendMessage(widget->window, CB_GETITEMHEIGHT, (WPARAM)-1, 0));
+    SendMessage(widget->window, CB_SETITEMHEIGHT, (WPARAM)-1, adjustedHeight);
 
-  if(*text) {
-    lstring list;
-    list.split("\n", text);
-    foreach(item, list) addItem(item);
-  }
+    if (*text) {
+        lstring list;
+        list.split("\n", text);
+        foreach (item, list) {
+            addItem(item);
+        }
+    }
 }
 
-void ComboBox::reset() {
-  SendMessage(widget->window, CB_RESETCONTENT, 0, 0);
+void ComboBox::reset(void)
+{
+    SendMessage(widget->window, CB_RESETCONTENT, 0, 0);
 }
 
-void ComboBox::addItem(const string &text) {
-  SendMessage(widget->window, CB_ADDSTRING, 0, (LPARAM)(wchar_t*)utf16_t(text));
-  if(SendMessage(widget->window, CB_GETCOUNT, 0, 0) == 1) setSelection(0);
+void ComboBox::addItem(const string &text)
+{
+    SendMessage(widget->window, CB_ADDSTRING, 0,
+        (LPARAM)(wchar_t*)utf16_t(text));
+    if (SendMessage(widget->window, CB_GETCOUNT, 0, 0) == 1) {
+        setSelection(0);
+    }
 }
 
-unsigned ComboBox::selection() {
-  return SendMessage(widget->window, CB_GETCURSEL, 0, 0);
+unsigned ComboBox::selection(void)
+{
+    return SendMessage(widget->window, CB_GETCURSEL, 0, 0);
 }
 
-void ComboBox::setSelection(unsigned row) {
-  SendMessage(widget->window, CB_SETCURSEL, comboBox->selection = row, 0);
+void ComboBox::setSelection(unsigned row)
+{
+    SendMessage(widget->window, CB_SETCURSEL, comboBox->selection = row, 0);
 }
 
-ComboBox::ComboBox() {
-  comboBox = new ComboBox::Data;
-  comboBox->selection = 0;
+ComboBox::ComboBox(void)
+{
+    comboBox = new ComboBox::Data;
+    comboBox->selection = 0;
 }
--- bsnes/phoenix/windows/menu.cpp
+++ bsnes/phoenix/windows/menu.cpp
@@ -1,144 +1,210 @@
-Action::Action() {
-  OS::os->objects.append(this);
-  action = new Action::Data;
+Action::Action(void)
+{
+    OS::os->objects.append(this);
+    action = new Action::Data;
 }
 
-void Menu::create(Window &parent, const string &text) {
-  action->parentMenu = parent.window->menu;
-  action->menu = CreatePopupMenu();
-  AppendMenu(parent.window->menu, MF_STRING | MF_POPUP, (UINT_PTR)action->menu, utf16_t(text));
+void Menu::create(Window &parent, const string &text)
+{
+    action->parentMenu = parent.window->menu;
+    action->menu = CreatePopupMenu();
+    AppendMenu(parent.window->menu, MF_STRING | MF_POPUP,
+        (UINT_PTR)action->menu, utf16_t(text));
 }
 
-void Menu::create(Menu &parent, const string &text) {
-  action->parentMenu = parent.action->menu;
-  action->menu = CreatePopupMenu();
-  AppendMenu(parent.action->menu, MF_STRING | MF_POPUP, (UINT_PTR)action->menu, utf16_t(text));
+void Menu::create(Menu &parent, const string &text)
+{
+    action->parentMenu = parent.action->menu;
+    action->menu = CreatePopupMenu();
+    AppendMenu(parent.action->menu, MF_STRING | MF_POPUP,
+        (UINT_PTR)action->menu, utf16_t(text));
 }
 
-bool Menu::enabled() {
-  MENUITEMINFO info;
-  memset(&info, 0, sizeof(MENUITEMINFO));
-  info.cbSize = sizeof(MENUITEMINFO);
-  info.fMask = MIIM_STATE;
-  GetMenuItemInfo(action->parentMenu, (UINT_PTR)action->menu, false, &info);
-  return (info.fState & MFS_GRAYED) == 0;
+bool Menu::enabled(void)
+{
+    MENUITEMINFO info;
+    memset(&info, 0, sizeof(MENUITEMINFO));
+    info.cbSize = sizeof(MENUITEMINFO);
+    info.fMask = MIIM_STATE;
+    GetMenuItemInfo(action->parentMenu, (UINT_PTR)action->menu, false, &info);
+    return (info.fState & MFS_GRAYED) == 0;
 }
 
-void Menu::setEnabled(bool enabled) {
-  EnableMenuItem(action->parentMenu, (UINT_PTR)action->menu, MF_BYCOMMAND | (enabled ? MF_ENABLED : MF_GRAYED));
+void Menu::setEnabled(bool enabled)
+{
+    if (enabled) {
+        EnableMenuItem(action->parentMenu, (UINT_PTR)action->menu,
+            MF_BYCOMMAND | MF_ENABLED);
+    } else {
+        EnableMenuItem(action->parentMenu, (UINT_PTR)action->menu,
+            MF_BYCOMMAND | MF_GRAYED);
+    }
 }
 
-void MenuSeparator::create(Menu &parent) {
-  action->parent = &parent;
-  AppendMenu(parent.action->menu, MF_SEPARATOR, object->id, L"");
+void MenuSeparator::create(Menu &parent)
+{
+    action->parent = &parent;
+    AppendMenu(parent.action->menu, MF_SEPARATOR, object->id, L"");
 }
 
-bool MenuSeparator::enabled() {
-  MENUITEMINFO info;
-  memset(&info, 0, sizeof(MENUITEMINFO));
-  info.cbSize = sizeof(MENUITEMINFO);
-  info.fMask = MIIM_STATE;
-  GetMenuItemInfo(action->parent->action->menu, object->id, false, &info);
-  return (info.fState & MFS_GRAYED) == 0;
+bool MenuSeparator::enabled(void)
+{
+    MENUITEMINFO info;
+    memset(&info, 0, sizeof(MENUITEMINFO));
+
+    info.cbSize = sizeof(MENUITEMINFO);
+    info.fMask = MIIM_STATE;
+    GetMenuItemInfo(action->parent->action->menu, object->id, false, &info);
+    return (info.fState & MFS_GRAYED) == 0;
 }
 
-void MenuSeparator::setEnabled(bool enabled) {
-  EnableMenuItem(action->parent->action->menu, object->id, MF_BYCOMMAND | (enabled ? MF_ENABLED : MF_GRAYED));
+void MenuSeparator::setEnabled(bool enabled)
+{
+    if (enabled) {
+        EnableMenuItem(action->parent->action->menu, object->id,
+            MF_BYCOMMAND | MF_ENABLED);
+    } else {
+        EnableMenuItem(action->parent->action->menu, object->id,
+            MF_BYCOMMAND | MF_GRAYED);
+    }
 }
 
-void MenuItem::create(Menu &parent, const string &text) {
-  action->parent = &parent;
-  AppendMenu(parent.action->menu, MF_STRING, object->id, utf16_t(text));
+void MenuItem::create(Menu &parent, const string &text)
+{
+    action->parent = &parent;
+    AppendMenu(parent.action->menu, MF_STRING, object->id, utf16_t(text));
 }
 
-bool MenuItem::enabled() {
-  MENUITEMINFO info;
-  memset(&info, 0, sizeof(MENUITEMINFO));
-  info.cbSize = sizeof(MENUITEMINFO);
-  info.fMask = MIIM_STATE;
-  GetMenuItemInfo(action->parent->action->menu, object->id, false, &info);
-  return (info.fState & MFS_GRAYED) == 0;
+bool MenuItem::enabled(void)
+{
+    MENUITEMINFO info;
+    memset(&info, 0, sizeof(MENUITEMINFO));
+
+    info.cbSize = sizeof(MENUITEMINFO);
+    info.fMask = MIIM_STATE;
+    GetMenuItemInfo(action->parent->action->menu, object->id, false, &info);
+    return (info.fState & MFS_GRAYED) == 0;
 }
 
-void MenuItem::setEnabled(bool enabled) {
-  EnableMenuItem(action->parent->action->menu, object->id, MF_BYCOMMAND | (enabled ? MF_ENABLED : MF_GRAYED));
+void MenuItem::setEnabled(bool enabled)
+{
+    if (enabled) {
+        EnableMenuItem(action->parent->action->menu, object->id,
+            MF_BYCOMMAND | MF_ENABLED);
+    } else {
+        EnableMenuItem(action->parent->action->menu, object->id,
+            MF_BYCOMMAND | MF_GRAYED);
+    }
 }
 
-void MenuCheckItem::create(Menu &parent, const string &text) {
-  action->parent = &parent;
-  AppendMenu(parent.action->menu, MF_STRING, object->id, utf16_t(text));
+void MenuCheckItem::create(Menu &parent, const string &text)
+{
+    action->parent = &parent;
+    AppendMenu(parent.action->menu, MF_STRING, object->id, utf16_t(text));
 }
 
-bool MenuCheckItem::enabled() {
-  MENUITEMINFO info;
-  memset(&info, 0, sizeof(MENUITEMINFO));
-  info.cbSize = sizeof(MENUITEMINFO);
-  info.fMask = MIIM_STATE;
-  GetMenuItemInfo(action->parent->action->menu, object->id, false, &info);
-  return (info.fState & MFS_GRAYED) == 0;
+bool MenuCheckItem::enabled(void)
+{
+    MENUITEMINFO info;
+    memset(&info, 0, sizeof(MENUITEMINFO));
+
+    info.cbSize = sizeof(MENUITEMINFO);
+    info.fMask = MIIM_STATE;
+    GetMenuItemInfo(action->parent->action->menu, object->id, false, &info);
+    return (info.fState & MFS_GRAYED) == 0;
 }
 
-void MenuCheckItem::setEnabled(bool enabled) {
-  EnableMenuItem(action->parent->action->menu, object->id, MF_BYCOMMAND | (enabled ? MF_ENABLED : MF_GRAYED));
+void MenuCheckItem::setEnabled(bool enabled)
+{
+    if (enabled) {
+        EnableMenuItem(action->parent->action->menu, object->id,
+            MF_BYCOMMAND | MF_ENABLED);
+    } else {
+        EnableMenuItem(action->parent->action->menu, object->id,
+            MF_BYCOMMAND | MF_GRAYED);
+    }
 }
 
-bool MenuCheckItem::checked() {
-  MENUITEMINFO info;
-  memset(&info, 0, sizeof(MENUITEMINFO));
-  info.cbSize = sizeof(MENUITEMINFO);
-  info.fMask = MIIM_STATE;
-  GetMenuItemInfo(action->parent->action->menu, object->id, false, &info);
-  return info.fState & MFS_CHECKED;
+bool MenuCheckItem::checked(void)
+{
+    MENUITEMINFO info;
+    memset(&info, 0, sizeof(MENUITEMINFO));
+
+    info.cbSize = sizeof(MENUITEMINFO);
+    info.fMask = MIIM_STATE;
+    GetMenuItemInfo(action->parent->action->menu, object->id, false, &info);
+    return info.fState & MFS_CHECKED;
 }
 
-void MenuCheckItem::setChecked(bool checked) {
-  CheckMenuItem(action->parent->action->menu, object->id, checked ? MF_CHECKED : MF_UNCHECKED);
+void MenuCheckItem::setChecked(bool checked)
+{
+    if (checked) {
+        CheckMenuItem(action->parent->action->menu, object->id, MF_CHECKED);
+    } else {
+        CheckMenuItem(action->parent->action->menu, object->id, MF_UNCHECKED);
+
+    }
 }
 
-void MenuRadioItem::create(Menu &parent, const string &text) {
-  action->parent = &parent;
-  action->radioParent = this;
-  action->items.append(this);
-  AppendMenu(parent.action->menu, MF_STRING, object->id, utf16_t(text));
-  setChecked();
+void MenuRadioItem::create(Menu &parent, const string &text)
+{
+    action->parent = &parent;
+    action->radioParent = this;
+    action->items.append(this);
+    AppendMenu(parent.action->menu, MF_STRING, object->id, utf16_t(text));
+    setChecked();
 }
 
-void MenuRadioItem::create(MenuRadioItem &parent, const string &text) {
-  action->parent = parent.action->parent;
-  action->radioParent = parent.action->radioParent;
-  action->radioParent->action->items.append(this);
-  AppendMenu(action->parent->action->menu, MF_STRING, object->id, utf16_t(text));
+void MenuRadioItem::create(MenuRadioItem &parent, const string &text)
+{
+    action->parent = parent.action->parent;
+    action->radioParent = parent.action->radioParent;
+    action->radioParent->action->items.append(this);
+    AppendMenu(action->parent->action->menu, MF_STRING,
+        object->id, utf16_t(text));
 }
 
-bool MenuRadioItem::enabled() {
-  MENUITEMINFO info;
-  memset(&info, 0, sizeof(MENUITEMINFO));
-  info.cbSize = sizeof(MENUITEMINFO);
-  info.fMask = MIIM_STATE;
-  GetMenuItemInfo(action->parent->action->menu, object->id, false, &info);
-  return (info.fState & MFS_GRAYED) == 0;
+bool MenuRadioItem::enabled(void)
+{
+    MENUITEMINFO info;
+    memset(&info, 0, sizeof(MENUITEMINFO));
+
+    info.cbSize = sizeof(MENUITEMINFO);
+    info.fMask = MIIM_STATE;
+    GetMenuItemInfo(action->parent->action->menu, object->id, false, &info);
+    return (info.fState & MFS_GRAYED) == 0;
 }
 
-void MenuRadioItem::setEnabled(bool enabled) {
-  EnableMenuItem(action->parent->action->menu, object->id, MF_BYCOMMAND | (enabled ? MF_ENABLED : MF_GRAYED));
+void MenuRadioItem::setEnabled(bool enabled)
+{
+    if (enabled) {
+        EnableMenuItem(action->parent->action->menu, object->id,
+            MF_BYCOMMAND | MF_ENABLED);
+    } else {
+        EnableMenuItem(action->parent->action->menu, object->id,
+            MF_BYCOMMAND | MF_GRAYED);
+    }
 }
 
-bool MenuRadioItem::checked() {
-  MENUITEMINFO info;
-  memset(&info, 0, sizeof(MENUITEMINFO));
-  info.cbSize = sizeof(MENUITEMINFO);
-  info.fMask = MIIM_STATE;
-  GetMenuItemInfo(action->parent->action->menu, object->id, false, &info);
-  return info.fState & MFS_CHECKED;
+bool MenuRadioItem::checked(void)
+{
+    MENUITEMINFO info;
+    memset(&info, 0, sizeof(MENUITEMINFO));
+
+    info.cbSize = sizeof(MENUITEMINFO);
+    info.fMask = MIIM_STATE;
+    GetMenuItemInfo(action->parent->action->menu, object->id, false, &info);
+    return info.fState & MFS_CHECKED;
 }
 
-void MenuRadioItem::setChecked() {
-  MenuRadioItem *parent = action->radioParent;
-  foreach(item, parent->action->items) {
-    CheckMenuRadioItem(
-      action->parent->action->menu,
-      item->object->id, item->object->id, item->object->id + (item != this),
-      MF_BYCOMMAND
-    );
-  }
+void MenuRadioItem::setChecked(void)
+{
+    MenuRadioItem *parent = action->radioParent;
+    foreach (item, parent->action->items) {
+        CheckMenuRadioItem(
+            action->parent->action->menu,
+            item->object->id, item->object->id,
+            item->object->id + (item != this), MF_BYCOMMAND
+        );
+    }
 }
