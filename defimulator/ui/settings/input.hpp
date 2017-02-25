ab2b30b4f18e5aeefb14a85ac738773d5254bebd
Joe Brown <brownj3199@live.com>
2017-02-25 05:07:39 -0600

    Cleanup.
    On the bright side, I figured out how to play Game Boy games!  Just
    load up the Super Game Boy rom, and then the GB rom.  Works for GBC
    as well.  Need to figure out how to uncouple that requirement though.

--- defimulator/ui/settings/input.hpp
+++ defimulator/ui/settings/input.hpp
@@ -1,34 +1,34 @@
 struct InputSettings : TopLevelWindow {
-  Label portLabel;
-  ComboBox portBox;
-  Label deviceLabel;
-  ComboBox deviceBox;
-  ListBox mappingList;
-  Button mouseXaxis;
-  Button mouseYaxis;
-  Button mouseLeft;
-  Button mouseMiddle;
-  Button mouseRight;
-  Button clearButton;
+    Label portLabel;
+    ComboBox portBox;
+    Label deviceLabel;
+    ComboBox deviceBox;
+    ListBox mappingList;
+    Button mouseXaxis;
+    Button mouseYaxis;
+    Button mouseLeft;
+    Button mouseMiddle;
+    Button mouseRight;
+    Button clearButton;
 
-  void inputEvent(uint16_t scancode, int16_t value);
-  void calibrateJoypads();
-  void create();
-  InputSettings();
+    void inputEvent(uint16_t scancode, int16_t value);
+    void calibrateJoypads(void);
+    void create(void);
+    InputSettings(void);
 
 private:
-  bool joypadsCalibrated;
-  bool joypadsCalibrating;
-  int16_t joypadCalibration[Joypad::Count][Joypad::Axes];
-  unsigned activeMouse;
+    bool joypadsCalibrated;
+    bool joypadsCalibrating;
+    int16_t joypadCalibration[Joypad::Count][Joypad::Axes];
+    unsigned activeMouse;
 
-  void portChanged();
-  void deviceChanged();
-  void mappingChanged();
-  void setMapping(const string &mapping);
-  void assignInput();
-  void clearInput();
-  void endAssignment();
+    void portChanged(void);
+    void deviceChanged(void);
+    void mappingChanged(void);
+    void setMapping(const string &mapping);
+    void assignInput(void);
+    void clearInput(void);
+    void endAssignment(void);
 };
 
 extern InputSettings inputSettings;
