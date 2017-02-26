c1d8de42fd41d1c1629661b197bb33c56d1c8c1e
Joe Brown <brownj3199@live.com>
2017-02-26 03:05:27 -0600

    Cleanup and execute bit removal.

--- defimulator/ui/general/file-browser.hpp
+++ defimulator/ui/general/file-browser.hpp
@@ -1,25 +1,33 @@
 struct FileBrowser : TopLevelWindow {
-  TextBox pathBox;
-  Button browseButton;
-  Button upButton;
-  ListBox contentsBox;
+    TextBox pathBox;
+    Button browseButton;
+    Button upButton;
+    ListBox contentsBox;
 
-  enum class Mode : unsigned { Cartridge, Satellaview, SufamiTurbo, GameBoy, Filter, Shader } mode;
-  void fileOpen(Mode mode, function<void (string)> callback);
-  void create();
+    enum class Mode : unsigned {
+        Cartridge,
+        Satellaview,
+        SufamiTurbo,
+        GameBoy,
+        Filter,
+        Shader
+    } mode;
+
+    void fileOpen(Mode mode, function<void (string)> callback);
+    void create(void);
 
 private:
-  function<void (string)> callback;
-  string folder;
-  lstring filters;
-  lstring contents;
+    function<void (string)> callback;
+    string folder;
+    lstring filters;
+    lstring contents;
 
-  void folderBrowse();
-  void folderUp();
-  void fileActivate();
-  void setFolder(const string &pathname);
-  string cartridgeFolder(const string &pathname);
-  void loadFile(const string &filename);
+    void folderBrowse(void);
+    void folderUp(void);
+    void fileActivate(void);
+    void setFolder(const string &pathname);
+    string cartridgeFolder(const string &pathname);
+    void loadFile(const string &filename);
 };
 
 extern FileBrowser fileBrowser;
--- defimulator/ui/general/slot-loader.hpp
+++ defimulator/ui/general/slot-loader.hpp
@@ -1,37 +1,42 @@
 struct SingleSlotLoader : TopLevelWindow {
-  Label baseLabel;
-  TextBox basePath;
-  Button baseBrowse;
-  Label slotLabel;
-  TextBox slotPath;
-  Button slotBrowse;
-  Button okButton;
+    Label baseLabel;
+    TextBox basePath;
+    Button baseBrowse;
+    Label slotLabel;
+    TextBox slotPath;
+    Button slotBrowse;
+    Button okButton;
 
-  void create();
-  void loadCartridgeBsxSlotted();
-  void loadCartridgeBsx();
-  void loadCartridgeSuperGameBoy();
+    void create(void);
+    void loadCartridgeBsxSlotted(void);
+    void loadCartridgeBsx(void);
+    void loadCartridgeSuperGameBoy(void);
 
-  enum class Mode : unsigned { BsxSlotted, Bsx, SuperGameBoy } mode;
-  void load();
+    enum class Mode : unsigned {
+        BsxSlotted,
+        Bsx,
+        SuperGameBoy
+    } mode;
+
+    void load(void);
 };
 
 struct DoubleSlotLoader : TopLevelWindow {
-  Label baseLabel;
-  TextBox basePath;
-  Button baseBrowse;
-  Label slotALabel;
-  TextBox slotAPath;
-  Button slotABrowse;
-  Label slotBLabel;
-  TextBox slotBPath;
-  Button slotBBrowse;
-  Button okButton;
+    Label baseLabel;
+    TextBox basePath;
+    Button baseBrowse;
+    Label slotALabel;
+    TextBox slotAPath;
+    Button slotABrowse;
+    Label slotBLabel;
+    TextBox slotBPath;
+    Button slotBBrowse;
+    Button okButton;
 
-  void create();
-  void loadCartridgeSufamiTurbo();
+    void create(void);
+    void loadCartridgeSufamiTurbo(void);
 
-  void load();
+    void load(void);
 };
 
 extern SingleSlotLoader singleSlotLoader;
