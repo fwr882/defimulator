76154420473eee1168bcba1a9892d1b1d29d8310
Joe Brown <brownj3199@live.com>
2017-03-04 11:45:15 -0600

    More cleanup.

--- defimulator/snes/audio/audio.cpp
+++ defimulator/snes/audio/audio.cpp
@@ -2,84 +2,92 @@
 
 Audio audio;
 
-void Audio::coprocessor_enable(bool state) {
-  coprocessor = state;
+void Audio::coprocessor_enable(bool state)
+{
+    coprocessor = state;
 
-  dsp_rdoffset = cop_rdoffset = 0;
-  dsp_wroffset = cop_wroffset = 0;
-  dsp_length = cop_length = 0;
+    dsp_rdoffset = 0;
+    cop_rdoffset = 0;
+    dsp_wroffset = 0;
+    cop_wroffset = 0;
+    dsp_length = 0;
+    cop_length = 0;
 
-  r_sum_l = r_sum_r = 0;
+    r_sum_l = 0;
+    r_sum_r = 0;
 }
 
-void Audio::coprocessor_frequency(double input_frequency) {
-  double output_frequency;
-  output_frequency = system.apu_frequency() / 768.0;
-  r_step = input_frequency / output_frequency;
-  r_frac = 0;
+void Audio::coprocessor_frequency(double input_frequency)
+{
+    double output_frequency;
+    output_frequency = system.apu_frequency() / 768.0;
+    r_step = input_frequency / output_frequency;
+    r_frac = 0;
 }
 
-void Audio::sample(int16 left, int16 right) {
-  if(coprocessor == false) {
-    system.interface->audio_sample(left, right);
-  } else {
-    dsp_buffer[dsp_wroffset] = ((uint16)left << 0) + ((uint16)right << 16);
-    dsp_wroffset = (dsp_wroffset + 1) & 32767;
-    dsp_length = (dsp_length + 1) & 32767;
+void Audio::sample(int16 left, int16 right)
+{
+    if (coprocessor == false) {
+        system.interface->audio_sample(left, right);
+    } else {
+        dsp_buffer[dsp_wroffset] = ((uint16)left << 0) + ((uint16)right << 16);
+        dsp_wroffset = (dsp_wroffset + 1) & 32767;
+        dsp_length = (dsp_length + 1) & 32767;
+        flush();
+    }
+}
+
+void Audio::coprocessor_sample(int16 left, int16 right)
+{
+    if (r_frac >= 1.0) {
+        r_frac -= 1.0;
+        r_sum_l += left;
+        r_sum_r += right;
+        return;
+    }
+
+    r_sum_l += left * r_frac;
+    r_sum_r += right * r_frac;
+
+    uint16 output_left  = sclamp<16>(int(r_sum_l / r_step));
+    uint16 output_right = sclamp<16>(int(r_sum_r / r_step));
+
+    double first = 1.0 - r_frac;
+    r_sum_l = left * first;
+    r_sum_r = right * first;
+    r_frac = r_step - first;
+
+    cop_buffer[cop_wroffset] = (output_left << 0) + (output_right << 16);
+    cop_wroffset = (cop_wroffset + 1) & 32767;
+    cop_length = (cop_length + 1) & 32767;
     flush();
-  }
 }
 
-void Audio::coprocessor_sample(int16 left, int16 right) {
-  if(r_frac >= 1.0) {
-    r_frac -= 1.0;
-    r_sum_l += left;
-    r_sum_r += right;
-    return;
-  }
+void Audio::init(void) { }
 
-  r_sum_l += left  * r_frac;
-  r_sum_r += right * r_frac;
+void Audio::flush(void)
+{
+    while (dsp_length > 0 && cop_length > 0) {
+        uint32 dsp_sample = dsp_buffer[dsp_rdoffset];
+        uint32 cop_sample = cop_buffer[cop_rdoffset];
 
-  uint16 output_left  = sclamp<16>(int(r_sum_l / r_step));
-  uint16 output_right = sclamp<16>(int(r_sum_r / r_step));
+        dsp_rdoffset = (dsp_rdoffset + 1) & 32767;
+        cop_rdoffset = (cop_rdoffset + 1) & 32767;
 
-  double first = 1.0 - r_frac;
-  r_sum_l = left  * first;
-  r_sum_r = right * first;
-  r_frac = r_step - first;
+        dsp_length--;
+        cop_length--;
 
-  cop_buffer[cop_wroffset] = (output_left << 0) + (output_right << 16);
-  cop_wroffset = (cop_wroffset + 1) & 32767;
-  cop_length = (cop_length + 1) & 32767;
-  flush();
-}
+        int dsp_left  = (int16)(dsp_sample >>  0);
+        int dsp_right = (int16)(dsp_sample >> 16);
 
-void Audio::init() {
-}
+        int cop_left  = (int16)(cop_sample >>  0);
+        int cop_right = (int16)(cop_sample >> 16);
 
-void Audio::flush() {
-  while(dsp_length > 0 && cop_length > 0) {
-    uint32 dsp_sample = dsp_buffer[dsp_rdoffset];
-    uint32 cop_sample = cop_buffer[cop_rdoffset];
-
-    dsp_rdoffset = (dsp_rdoffset + 1) & 32767;
-    cop_rdoffset = (cop_rdoffset + 1) & 32767;
-
-    dsp_length--;
-    cop_length--;
-
-    int dsp_left  = (int16)(dsp_sample >>  0);
-    int dsp_right = (int16)(dsp_sample >> 16);
-
-    int cop_left  = (int16)(cop_sample >>  0);
-    int cop_right = (int16)(cop_sample >> 16);
-
-    system.interface->audio_sample(
-      sclamp<16>((dsp_left  + cop_left ) / 2),
-      sclamp<16>((dsp_right + cop_right) / 2)
-    );
-  }
+        system.interface->audio_sample(
+            sclamp<16>((dsp_left  + cop_left ) / 2),
+            sclamp<16>((dsp_right + cop_right) / 2)
+        );
+    }
 }
 
 #endif
--- defimulator/snes/audio/audio.hpp
+++ defimulator/snes/audio/audio.hpp
@@ -1,22 +1,32 @@
 class Audio {
 public:
-  void coprocessor_enable(bool state);
-  void coprocessor_frequency(double frequency);
-  void sample(int16 left, int16 right);
-  void coprocessor_sample(int16 left, int16 right);
-  void init();
+    void coprocessor_enable(bool state);
+    void coprocessor_frequency(double frequency);
+    void sample(int16 left, int16 right);
+    void coprocessor_sample(int16 left, int16 right);
+    void init(void);
 
 private:
-  bool coprocessor;
-  uint32 dsp_buffer[32768], cop_buffer[32768];
-  unsigned dsp_rdoffset, cop_rdoffset;
-  unsigned dsp_wroffset, cop_wroffset;
-  unsigned dsp_length, cop_length;
+    bool coprocessor;
 
-  double r_step, r_frac;
-  int r_sum_l, r_sum_r;
+    uint32 dsp_buffer[32768];
+    uint32 cop_buffer[32768];
 
-  void flush();
+    unsigned dsp_rdoffset;
+    unsigned cop_rdoffset;
+
+    unsigned dsp_wroffset;
+    unsigned cop_wroffset;
+
+    unsigned dsp_length;
+    unsigned cop_length;
+
+    double r_step;
+    double r_frac;
+    int r_sum_l;
+    int r_sum_r;
+
+    void flush(void);
 };
 
 extern Audio audio;
