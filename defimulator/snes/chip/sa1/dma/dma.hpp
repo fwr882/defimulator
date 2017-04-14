75dfd0561c8ccbda777397bbdd2c016dab6c4eeb
Joe Brown <brownj3199@live.com>
2017-04-14 13:12:38 -0500

    Fixed compilation warning for a bool in a switch.
    While I was there, did some cleanup of that code.  Most appears to be
    re-written but is actually just improved styling.

--- defimulator/snes/chip/sa1/dma/dma.hpp
+++ defimulator/snes/chip/sa1/dma/dma.hpp
@@ -1,11 +1,29 @@
+#ifndef DEFIMULATOR_SNES_CHIP_SA1_DMA_H
+#define DEFIMULATOR_SNES_CHIP_SA1_DMA_H
+
 struct DMA {
-  enum CDEN { DmaNormal = 0, DmaCharConversion = 1 };
-  enum SD { SourceROM = 0, SourceBWRAM = 1, SourceIRAM = 2 };
-  enum DD { DestIRAM = 0, DestBWRAM = 1 };
-  unsigned line;
+    enum CDEN {
+        DmaNormal = 0,
+        DmaCharConversion = 1
+    };
+
+    enum SD {
+        SourceROM = 0,
+        SourceBWRAM = 1,
+        SourceIRAM = 2
+    };
+
+    enum DD {
+        DestIRAM = 0,
+        DestBWRAM = 1
+    };
+
+    unsigned line;
 } dma;
 
-void dma_normal();
-void dma_cc1();
+void dma_normal(void);
+void dma_cc1(void);
 uint8 dma_cc1_read(unsigned addr);
-void dma_cc2();
+void dma_cc2(void);
+
+#endif
