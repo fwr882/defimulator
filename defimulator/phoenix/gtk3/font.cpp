c7c7bf18253b2cd4d47edf9305df92577d71b942
Joe Brown <grim210@users.noreply.github.com>
2017-04-30 16:12:13 -0500

    GTK3 port builds!
    Because I turned all the methods into code-less zombies.  But I feel
    a re-write from the ground up to be able to do this properly.

--- defimulator/phoenix/gtk3/font.cpp
+++ defimulator/phoenix/gtk3/font.cpp
@@ -1,36 +1,12 @@
 bool Font::create(const string &name, unsigned size, Font::Style style)
 {
-    font->font = pango_font_description_new();
-
-    pango_font_description_set_family(font->font, name);
-    pango_font_description_set_size(font->font, size * PANGO_SCALE);
-
-    if ((style & Style::Italic) == Style::Italic) {
-        pango_font_description_set_style(font->font, PANGO_STYLE_OBLIQUE);
-    } else {
-        pango_font_description_set_style(font->font, PANGO_STYLE_NORMAL);
-    }
-
-    if ((style & Style::Bold) == Style::Bold) {
-        pango_font_description_set_weight(font->font, PANGO_WEIGHT_BOLD);
-    } else {
-        pango_font_description_set_weight(font->font, PANGO_WEIGHT_NORMAL);
-    }
-
-    return true;
+    return false;
 }
 
 Font::Font(void)
 {
-    font = new Font::Data;
-    font->font = 0;
 }
 
 Font::~Font(void)
 {
-    if (font->font) {
-        pango_font_description_free(font->font);
-    }
-
-    delete font;
 }
